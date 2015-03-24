5c97b0f501c20e461a2a353caa03c8dff5f95e05
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-24 17:16:00 +0100

    + Create initial karma config.

---
+++ karma.conf.js
@@ -0,0 +1,79 @@
+// Karma configuration
+// Generated on Tue Mar 24 2015 16:42:40 GMT+0100 (WAT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      './libs/angular/angular.js',
+      './libs/angular-mocks/angular-mocks.js',
+      './libs/angular-route/angular-route.js',
+      './libs/satellizer/satellizer.js',
+      './libs/sloc-bower/sloc.js',
+      './libs/base-64/base64.js',
+      './libs/angular-chartist/dist/angular-chartist.js',
+      './app/core/core.module.js',
+      './app/core/services/authentication.services.js',
+      './app/core/controllers/authentication.controller.js',
+      './tests/unit/core/services/**/*.js',
+      './tests/unit/core/controllers/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+        './app/core/core.module.js': ['coverage'],
+        './app/core/services/authentication.service.js': ['coverage'],
+        './app/core/controllers/authentication.controller.js': ['coverage']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
